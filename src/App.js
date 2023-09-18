1d45225bf9665ec7b871f443c088839d901c32bc
Erhan Fırat <erhanfirat@gmail.com>
2023-09-18 12:57:04 +0300

    app js içeriğini türkçeye çevirdim

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Client Auth Projesi: Friends</h1>
+      <h1>Kullanıcı kimlik doğrulama: Arkadaşlar</h1>
     </div>
   );
 }
